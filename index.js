23608a6cb0889a255b8aee62b1e98181c2995399
Daniel-Derhami <daniel.derhami@gmail.com>
2023-06-06 19:38:48 +0200

    js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const video = document.getElementById('video');
 const videoControls = document.getElementById('video-controls');
+const playButton = document.getElementById('play');
+const playbackIcons = document.querySelectorAll('.playback-icons use');
