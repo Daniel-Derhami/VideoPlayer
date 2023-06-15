3b20691f91fe2832f0b65f394521f25028f10d4b
Daniel-Derhami <daniel.derhami@gmail.com>
2023-06-15 16:40:17 +0200

    JS

--- index.js
+++ index.js
@@ -44,3 +44,15 @@ function updatePlayButton() {
     playButton.setAttribute('data-title', 'Pause (k)');
   }
 }
+
+
+
+
+function formatTime(timeInSeconds) {
+  const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+  return {
+    minutes: result.substr(3, 2),
+    seconds: result.substr(6, 2),
+  };
+}
